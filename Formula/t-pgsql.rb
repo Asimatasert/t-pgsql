037c3582076ca07cfc6762b3fc99bce5d8773130
asim.atasert <asimatasert@outlook.com>
2026-01-05 14:18:29 +0300

    chore: Update Homebrew formula for v3.3.0

--- Formula/t-pgsql.rb
+++ Formula/t-pgsql.rb
@@ -4,8 +4,8 @@
 class TPgsql < Formula
   desc "PostgreSQL database sync, backup and clone tool with notifications"
   homepage "https://github.com/Asimatasert/t-pgsql"
-  url "https://github.com/Asimatasert/t-pgsql/archive/refs/tags/v3.2.0.tar.gz"
-  sha256 "4253ace1a7393efeecd771d6b919b4ae506f150534e12672f88a3060d82745fd"
+  url "https://github.com/Asimatasert/t-pgsql/archive/refs/tags/v3.3.0.tar.gz"
+  sha256 "2ec8d0fc1d6f4eb20e0cd68fb91459957c184a8b7fb959b923a6016d7b887ad3"
   license "MIT"
   head "https://github.com/Asimatasert/t-pgsql.git", branch: "master"
 
@@ -17,6 +17,6 @@ class TPgsql < Formula
   end
 
   test do
-    assert_match "v3.2.0", shell_output("#{bin}/t-pgsql --version")
+    assert_match "v3.3.0", shell_output("#{bin}/t-pgsql --version")
   end
 end
